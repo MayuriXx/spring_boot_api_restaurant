3c170c1a3f0e894bf82e6099c3ea85e2180f74aa
MayuriXx <martho.evan@gmail.com>
2021-01-13 20:18:54 +0100

    feat : add restaurant service and impl

--- src/main/java/com/formation/restaurant/controlers/RestaurantController.java
+++ src/main/java/com/formation/restaurant/controlers/RestaurantController.java
@@ -1,6 +1,8 @@
 package com.formation.restaurant.controlers;
 
 import com.formation.restaurant.models.Restaurant;
+import com.formation.restaurant.services.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,19 +15,11 @@ import java.util.List;
 @RequestMapping("/restaurants")
 public class RestaurantController {
 
+    @Autowired
+    private RestaurantService restaurantService;
+
     @GetMapping
-    public List<Restaurant> findAll(){
-        List<Restaurant> list = new ArrayList<Restaurant>();
-        Restaurant r1 = new Restaurant();
-        r1.setId("1");
-        r1.setName("test1");
-        r1.setAddress("test1 address");
-        list.add(r1);
-        Restaurant r2 = new Restaurant();
-        r2.setId("2");
-        r2.setName("test2");
-        r2.setAddress("test2 address");
-        list.add(r2);
-        return list;
+    public List<Restaurant> findAll() {
+        return restaurantService.findAll();
     }
 }
---
+++ src/main/java/com/formation/restaurant/services/RestaurantService.java
@@ -0,0 +1,10 @@
+package com.formation.restaurant.services;
+
+import com.formation.restaurant.models.Restaurant;
+
+import java.util.List;
+
+public interface RestaurantService {
+
+    public List<Restaurant> findAll();
+}
